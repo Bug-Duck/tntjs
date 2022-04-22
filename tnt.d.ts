4d4de0e463f60c89978b7f12336394be3dcc7f4c
sheepbox8646 <78635021+sheepbox8646@users.noreply.github.com>
2022-04-22 21:37:24 +0800

    完成(不确定有没有Bug)

--- tnt.d.ts
+++ tnt.d.ts
@@ -13,16 +13,17 @@ declare let TNTSymbolTable: {
     sleep: (x: any) => Promise<unknown>;
 };
 declare function JsTypeToTNTType(TypeName: any): "string" | "number" | "bool";
+declare function newData(type: ('string' | 'number' | 'tnt' | 'boolean' | 'function'), name: string, value: any, datahouse: {}): void;
 declare function TNTMatchStartSymbol(startSymbol: any, endSymbol: any, data: any, startIndex: any): any;
-declare function TNTValueProcess(reg: any): any;
-declare function TNTBoom(codeList: any, data?: any, isinclass?: boolean): any;
-declare function TNTFunctionSplit(code: any, original?: boolean): {
+declare function TNTValueProcess(reg: string): any;
+declare function TNTBoom(codeList: string[], data?: any, isinclass?: boolean): any;
+declare function TNTFunctionSplit(code: string, original?: boolean): {
     agv: any[];
     functioncanvalue: {};
 };
-declare function def(func_data: any, In_data: any): void;
-declare function render(HTML: any, DOM: any): void;
-declare function TNTCodeSplit(code: any): any[];
+declare function def(func_data: string, In_data: string[]): void;
+declare function render(HTML: string, DOM: any): void;
+declare function TNTCodeSplit(code: string): any[];
 declare function TNTValueTagProcessing(): void;
 declare function TNTValueTagValueRenderer(tagValue: any): any;
 declare function TNTTagProcessing(): void;
