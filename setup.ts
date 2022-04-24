846110d61f7cdec49dec5ff813b5a94f16662eb0
27Onion Nebell <57032603+onion108@users.noreply.github.com>
2022-04-24 11:27:29 +0800

    Fixed bug

--- setup.ts
+++ setup.ts
@@ -1,6 +1,10 @@
 window.onload = () => {
-    TNT.TNTValueTagProcessing();
     TNT.TNTTagProcessing();
+    TNT.TNTValueTagProcessing();
+    TNT.TNTSymbolTable["explorerType"] = {
+        type: 'string',
+        value: TNT.TNTGetBrowserType(),
+    } as TNT.TNTData;
     setTimeout(() => {
         TNT.TNTSymbolTable["test"].value = 114514;
         TNT.TNTValueTagProcessing();
