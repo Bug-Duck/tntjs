98f1640bbfcd9434de6b6f4c6c7a1db75c2e9b2a
sheepbox8646 <78635021+sheepbox8646@users.noreply.github.com>
2022-04-28 21:11:20 +0800

    f

--- tnt.ts
+++ tnt.ts
@@ -46,7 +46,7 @@ namespace TNT {
     }
 
     // This function evaluates the value of the expression.
-    function TNTValueProcess(reg: string) {
+    export function TNTValueProcess(reg: string) {
         // Regular Expression
         const isString = /(\"|\').+(\"|\')/;
         const isNumber = /[0-9]+/;
@@ -139,6 +139,7 @@ namespace TNT {
     function TNTBoom(codeList: string[], data: any = {}, isinclass: boolean = false) {
         let index = 0;
         const TNTSymbolTableOWN = data; // Inner data space
+        let Errors: TNTError = new TNTError(TNTSymbolTableOWN.__selfdom__)
         for (const code of codeList) {
             if (/([A-z0-9])+ ?= ?.+/.test(code)) { // Variable assignment statement
                 const name = /[^? =]/.exec(/([A-z0-9])+ ?=/.exec(code).join(' '));
@@ -164,7 +165,7 @@ namespace TNT {
             } else if (/(for|while|def|render) .+/.test(code)) {
                 if (/render/.test(code)) {
                     let html = code.replace(/render /, '');
-                    TNTRenderDOM(html, TNTSymbolTableOWN.__slefdom__)
+                    TNTRenderDOM(html, TNTSymbolTableOWN.__selfdom__)
                 } else if (/while/.test(code)) {
                     const YesorNo = TNTValueProcess((/([^while ]).+/.exec(code))[0]);
                     if (YesorNo) {
@@ -180,79 +181,9 @@ namespace TNT {
                 TNTValueProcess(code);
             }
             index = index + 1;
+            Errors.line += 1;
         }
         return TNTSymbolTableOWN;
-    }
-
-    function TNTFunctionSplit(code: string, original: boolean = false) {
-        let ignoreNext = false;
-        let stringIgnoreNext = false;
-        const buffer = [];
-        let currentString = "";
-        // Iterate every charaters in the code
-        for (const i of code) {
-            // Next will be ignored (In this case, means in a string.)
-            if (ignoreNext) {
-                // Add the current char to the string.
-                currentString += i;
-                if (i === '\\') {
-                    // Escaping characters
-                    stringIgnoreNext = true;
-                    continue;
-                }
-                if (i === '"' && !stringIgnoreNext) {
-                    // End the string.
-                    ignoreNext = false;
-                }
-                if (stringIgnoreNext) {
-                    stringIgnoreNext = false;
-                }
-                continue;
-            } else {
-                if (i === ',') {
-                    buffer.push(currentString.trim());
-                    currentString = "";
-                } else if (i === '"') {
-                    currentString += i;
-                    ignoreNext = true;
-                } else {
-                    currentString += i;
-                }
-            }
-        }
-        if (currentString !== "") {
-            buffer.push(currentString.trim());
-            currentString = "";
-        }
-        const values = { agv: [], functioncanvalue: {} };
-        for (const value of buffer) {
-            if (/.+ ?= ?.+/.test(value)) {
-                const v = /[^= ]+/.exec(/= ?.+/.exec(code).join(' '));
-                const name = /[^? =]/.exec(/([A-z0-9])+ ?=/.exec(code).join(' '));
-                values.functioncanvalue[name[0]] = TNTValueProcess(v[0]);
-            } else {
-                if (original) {
-                    values.agv.push(TNTValueProcess(value));
-                } else {
-                    values.agv.push(value)
-                }
-            }
-        }
-        return values;
-    }
-
-    // This function is very important to def a TNT.js's function!
-    function TNTDefineFunction(func_data: string, In_data: string[]) {
-        const name = /[^\(.+\)]+/.exec(func_data)[0].replace(/\s*/g, "");
-        let __parameters__ = /\(.+\)/.exec(func_data);
-        let __parameter__ = __parameters__[0];
-        __parameter__ = __parameter__.substring(1, __parameter__.length - 1);
-        TNTSymbolTable[name] = {
-            type: "tnt",
-            parameter: TNTFunctionSplit(__parameter__, true),
-            canparameter: TNTFunctionSplit(__parameter__).functioncanvalue,
-            code: In_data
-        }
     }
 
     function TNTRenderDOM(HTML: string, DOM: HTMLElement) {
@@ -261,49 +192,15 @@ namespace TNT {
         TNTTagProcessing();
     }
 
-    // This function splits code with ";"
-    function TNTCodeSplit(code: string) {
-        let ignoreNext = false;
-        let stringIgnoreNext = false;
-        const buffer = [];
-        let currentString = "";
-        // Iterate every charaters in the code
-        for (const i of code) {
-            // Next will be ignored (In this case, means in a string.)
-            if (ignoreNext) {
-                // Add the current char to the string.
-                currentString += i;
-                if (i === '\\') {
-                    // Escaping characters
-                    stringIgnoreNext = true;
-                    continue;
-                }
-                if (i === '"' && !stringIgnoreNext) {
-                    // End the string.
-                    ignoreNext = false;
-                }
-                if (stringIgnoreNext) {
-                    stringIgnoreNext = false;
-                }
-                continue;
-            } else {
-                if (i === ';') {
-                    buffer.push(currentString);
-                    currentString = "";
-                } else if (i === '"') {
-                    currentString += i;
-                    ignoreNext = true;
-                } else {
-                    currentString += i;
-                }
-            }
+    
+    // Rendering the <v> tag content to the value.
+    export function TNTValueTagValueRenderer(tagValue: string): any {
+        if (TNTSymbolTable[tagValue.trim()] === undefined) {
+            throw new Error(`Undefined TNT variable: ${tagValue.trim()}`);
         }
-        if (currentString !== "") {
-            buffer.push(currentString);
-            currentString = "";
-        }
-        return buffer;
+        return TNTSymbolTable[tagValue.trim()].value;
     }
+
 
     // Processes the <v></v> tag and replaces them into values
     export function TNTValueTagProcessing() {
@@ -320,14 +217,6 @@ namespace TNT {
                 va.innerHTML = `<span>${re}</span>`;
             }
         }
-    }
-
-    // Rendering the <v> tag content to the value.
-    function TNTValueTagValueRenderer(tagValue: string): any {
-        if (TNTSymbolTable[tagValue.trim()] === undefined) {
-            throw new Error(`Undefined TNT variable: ${tagValue.trim()}`);
-        }
-        return TNTSymbolTable[tagValue.trim()].value;
     }
 
     // Processes the <tnt> tag.
