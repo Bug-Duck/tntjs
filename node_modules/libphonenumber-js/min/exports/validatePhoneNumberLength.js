ae2de05ffe89d9c667665d70bc679d3aadbab996
BugDuck <850625057@qq.com>
2022-05-14 22:36:46 +0800

    删除 node_modules

--- node_modules/libphonenumber-js/min/exports/validatePhoneNumberLength.js
+++
@@ -1,6 +0,0 @@
-import { withMetadata } from '../metadata'
-import { validatePhoneNumberLength as _validatePhoneNumberLength } from '../../core/index'
-
-export function validatePhoneNumberLength() {
-	return withMetadata(_validatePhoneNumberLength, arguments)
-}
