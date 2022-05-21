d65035db62dda63c4871d18cf70888b460f35776
850625057 <850625057@qq.com>
2022-05-21 23:37:51 +0800

    build 95%

--- src/tntscript/ScriptExecutor.ts
+++ src/tntscript/ScriptExecutor.ts
@@ -1,6 +1,6 @@
 /**
  * file: ScriptExecutor.ts
- * creator: 27Onion
+ * creator: 27Onion, Acbox
  * create time: May 10th, 2022, 21:18
  * description: The TNT script executor.
  * main developer: sheepbox8646
@@ -13,10 +13,12 @@ namespace TNTScript {
     }
 
     export class ScriptExecutor {
+        TNTSymbolTableOWN: {};
+        SymbolTableOWN: Object;
         exec(scriptContent: string, data: Object = {}) {
-            const codeList = TNTScript.TNTCodeSplit(scriptContent);
+            const codeList = TNTScript.codeSplit(scriptContent);
             let index = 0;
-            const TNTSymbolTableOWN = data; // Inner data space
+            this.SymbolTableOWN = data; // Inner data space
             // let Errors: TNTError = new TNTError(TNTSymbolTableOWN.__selfdom__)
             for (const code of codeList) {
                 if (/([A-z0-9])+ ?= ?.+/.test(code)) { // Variable assignment statement
@@ -33,7 +35,7 @@ namespace TNTScript {
                         //     TNTSymbolTableOWN,
                         // );
                     } else {
-                        TNT.Globals.symbolTable.setValue(process.type,process.value)
+                        TNT.Globals.symbolTable.setValue(process.type, process.value)
                     }
                     // Refresh the page.
                     // TNTValueTagProcessing();
@@ -43,15 +45,16 @@ namespace TNTScript {
                         // TODO: 渲染render关键字
                         // TNTRenderDOM(html, TNTSymbolTableOWN.__selfdom__)
                     } else if (/while/.test(code)) {
-                        const i = codeKuai(keySearch('while', code));
+                        const i = TNTScript.codeBlock(keySearch('while', code));
                         const condition = i[0];
                         const codes = i[1];
                         // 如果TNTBoom函数返回true 即代表识别到跳出语句 则跳出循环
-                        while (this.ValueProcess(condition)) {
-                            if (this.exec(codes)) {
-                                break;
-                            }
-                        }
+                        // TODO: 这里类型监测出了点问题，因此注释
+                        // while (this.ValueProcess(condition)) {
+                        //     if (this.exec(codes)) {
+                        //         break;
+                        //     }
+                        // }
                     } else if (/def/.test(code)) {
                         // TODO: 定义函数语句
                     } else if (/for/.test(code)) {
@@ -66,7 +69,6 @@ namespace TNTScript {
                 index = index + 1;
                 // Errors.line += 1;
             }
-            return TNTSymbolTableOWN;
             // TODO: Fill in this function to execute the script. The content is given.
             // console.log(scriptContent);
         }
@@ -83,12 +85,12 @@ namespace TNTScript {
             if (/.+\(.+\)/.test(reg)) { // Interpreting function content
                 const name = /[^\(.+\)]+/.exec(reg)[0].replace(/\s*/g, "");
                 // TODO: Script类型type值
-                if (TNTSymbolTable[name].type === 'function') {
+                if (TNT.Globals.symbolTable.getValue(name).value === 'function') {
                     let buffer = "";
                     let __parameters__ = /\(.+\)/.exec(reg);
                     let __parameter__ = __parameters__[0];
                     __parameter__ = __parameter__.substring(1, __parameter__.length - 1);
-                    const parameters = TNTFunctionSplit(__parameter__)
+                    const parameters = TNTScript.functionSplit(__parameter__)
                     for (const i of parameters['agv']) {
                         buffer = buffer + i;
                         buffer = buffer + ',';
@@ -98,35 +100,35 @@ namespace TNTScript {
                     }
                     const results = Function(`TNT.TNTSymbolTable.${name}.value(${buffer})`)();
                     const result: value = {
-                        type: JsTypeToTNTType(typeof (results)),
+                        type: TNTScript.jsTypeToTNTType(typeof (results)),
                         value: results,
                     };
                     return result;
-                } else if (TNT.Globals.symbolTable.getValue(name) === 'tnt') {
+                } else if (TNT.Globals.symbolTable.getValue(name).value === 'tnt') {
                     // TODO:TNTScript函数调用
                     let __parameters__ = /\(.+\)/.exec(reg);
                     let __parameter__ = __parameters__[0];
                     __parameter__ = __parameter__.substring(1, __parameter__.length - 1);
-                    // Parameters is the arguments of the function.
-                    const parameters = TNTFunctionSplit(__parameter__);
+                    // TODO: Parameters is the arguments of the function.
+                    const parameters = TNTScript.functionSplit(__parameter__);
                     let par = {};
                     // Get the default parameters and the default values
-                    TNTSymbolTable[name].parameter.forEach((ele, i) => {
-                        par[ele] = parameters.agv[i];
-                    });
-                    for (const key in TNTSymbolTable[name].canparameter) {
-                        par[key] = TNTSymbolTable[name].canparameter[key];
-                    }
-                    // Merge optional parameters
-                    for (const key in TNTSymbolTable[name].canparameter) {
-                        par[key] = parameters.functioncanvalue[key];
-                    }
-                    this.exec(TNTSymbolTable[name].code, par)
+                    // TNTSymbolTable[name].parameter.forEach((ele, i) => {
+                    //     par[ele] = parameters.agv[i];
+                    // });
+                    // for (const key in TNTSymbolTable[name].canparameter) {
+                    //     par[key] = TNTSymbolTable[name].canparameter[key];
+                    // }
+                    // // Merge optional parameters
+                    // for (const key in TNTSymbolTable[name].canparameter) {
+                    //     par[key] = parameters.functioncanvalue[key];
+                    // }
+                    // this.exec(TNTSymbolTable[name].code, par)
                 }
             } else if (iscodes.test(reg)) {
                 return {
                     type: 'code',
-                    value: TNTCodeSplit(reg.substring(1, reg.length - 1)),
+                    value: TNTScript.codeSplit(reg.substring(1, reg.length - 1)),
                 }
             } else if (isNumber.test(reg)) {
                 // Number literal processing
@@ -144,9 +146,9 @@ namespace TNTScript {
                 };
             } else if (isVar.test(reg)) {
                 // Variable processing
-                const results = TNTSymbolTable[reg].value;
+                const results = TNT.Globals.symbolTable.getValue(reg).value;
                 const result: value = {
-                    type: JsTypeToTNTType(typeof (results)),
+                    type: TNTScript.jsTypeToTNTType(typeof (results)),
                     value: results,
                 };
                 return result;
@@ -159,156 +161,9 @@ namespace TNTScript {
                 }
                 return result;
             }
-        //     const codeList = TNTScript.TNTCodeSplit(scriptContent);
-        //     let index = 0;
-        //     const TNTSymbolTableOWN = data; // Inner data space
-        //     // let Errors: TNTError = new TNTError(TNTSymbolTableOWN.__selfdom__)
-        //     for (const code of codeList) {
-        //         if (/([A-z0-9])+ ?= ?.+/.test(code)) { // Variable assignment statement
-        //             const name = /[^? =]/.exec(/([A-z0-9])+ ?=/.exec(code).join(' '));
-        //             const v = /[^= ]+/.exec(/= ?.+/.exec(code).join(' '));
-        //             const process = this.ValueProcess(v[0]);
-        //             // TODO: 变量赋值的值重构后存储
-        //             // if (/let /.test(code)) {
-        //             //     newData(
-        //             //         process.type,
-        //             //         name[0],
-        //             //         process.value,
-        //             //         TNTSymbolTableOWN,
-        //             //     );
-        //             // } else {
-        //             //     newData(
-        //             //         process.type,
-        //             //         name[0],
-        //             //         process.value,
-        //             //         TNTSymbolTable,
-        //             //     );
-        //             // }
-        //             // // Refresh the page.
-        //             // TNTValueTagProcessing();
-        //         } else if (/(for|while|def|render) .+/.test(code)) {
-        //             if (/render/.test(code)) {
-        //                 const html = keySearch('render', code)
-        //                 // TODO: 渲染render关键字
-        //                 // TNTRenderDOM(html, TNTSymbolTableOWN.__selfdom__)
-        //             } else if (/while/.test(code)) {
-        //                 const i = codeKuai(keySearch('while', code));
-        //                 const condition = i[0];
-        //                 const codes = i[1];
-        //                 // 如果TNTBoom函数返回true 即代表识别到跳出语句 则跳出循环
-        //                 while (this.ValueProcess(condition)) {
-        //                     if (this.exec(codes)) {
-        //                         break;
-        //                     }
-        //                 }
-        //             } else if (/def/.test(code)) {
-        //                 // TODO: 定义函数语句
-        //             } else if (/for/.test(code)) {
-        //                 //TODO: for循环
-        //             }
-        //         } else if (code === 'break') {
-        //             // 如果检测到跳出循环语句 则返回true
-        //             return true
-        //         } else {
-        //             this.ValueProcess(code);
-        //         }
-        //         index = index + 1;
-        //         // Errors.line += 1;
-        //     }
-        //     return TNTSymbolTableOWN;
-        //     // TODO: Fill in this function to execute the script. The content is given.
-        //     // console.log(scriptContent);
-        // }
-
-        // ValueProcess(reg: string) {
-        //     // Regular Expression
-        //     const isString = /(\"|\').+(\"|\')/;
-        //     const isNumber = /[0-9]+/;
-        //     const isBool = /(true|false)/;
-        //     const isVar = /[_A-z0-9]/;
-        //     const isMathGex = /(.+ ?(\+|-|\*|\/)+ ?.+)+/;
-        //     const isXML = /<.+>/;
-        //     const iscodes = /\{.+\}/
-        //     if (/.+\(.+\)/.test(reg)) { // Interpreting function content
-        //         const name = /[^\(.+\)]+/.exec(reg)[0].replace(/\s*/g, "");
-        //         // TODO: Script类型type值
-        //         if (TNTSymbolTable[name].type === 'function') {
-        //             let buffer = "";
-        //             let __parameters__ = /\(.+\)/.exec(reg);
-        //             let __parameter__ = __parameters__[0];
-        //             __parameter__ = __parameter__.substring(1, __parameter__.length - 1);
-        //             const parameters = TNTFunctionSplit(__parameter__)
-        //             for (const i of parameters['agv']) {
-        //                 buffer = buffer + i;
-        //                 buffer = buffer + ',';
-        //             }
-        //             for (const i in parameters['functioncanvalue']) {
-        //                 buffer = buffer + i + '=' + ',';
-        //             }
-        //             const results = eval(`TNT.TNTSymbolTable.${name}.value(${buffer})`);
-        //             const result: value = {
-        //                 type: JsTypeToTNTType(typeof (results)),
-        //                 value: results,
-        //             };
-        //             return result;
-        //         } else if (TNTSymbolTable[name].type === 'tnt') {
-        //             // TODO:TNTScript函数调用
-        //             let __parameters__ = /\(.+\)/.exec(reg);
-        //             let __parameter__ = __parameters__[0];
-        //             __parameter__ = __parameter__.substring(1, __parameter__.length - 1);
-        //             // Parameters is the arguments of the function.
-        //             const parameters = TNTFunctionSplit(__parameter__);
-        //             let par = {};
-        //             // Get the default parameters and the default values
-        //             TNTSymbolTable[name].parameter.forEach((ele, i) => {
-        //                 par[ele] = parameters.agv[i];
-        //             });
-        //             for (const key in TNTSymbolTable[name].canparameter) {
-        //                 par[key] = TNTSymbolTable[name].canparameter[key];
-        //             }
-        //             // Merge optional parameters
-        //             for (const key in TNTSymbolTable[name].canparameter) {
-        //                 par[key] = parameters.functioncanvalue[key];
-        //             }
-        //             this.exec(TNTSymbolTable[name].code, par)
-        //         }
-        //     } else if (iscodes.test(reg)) {
-        //         return {
-        //             type: 'code',
-        //             value: TNTCodeSplit(reg.substring(1, reg.length - 1)),
-        //         }
-        //     } else if (isNumber.test(reg)) {
-        //         // Number literal processing
-        //         return {
-        //             type: 'number',
-        //             value: Number(reg)
-        //         };
-        //     } else if (isString.test(reg)) {
-        //         // TODO: Implement the string literal processing
-        //     } else if (isBool.test(reg)) {
-        //         // Boolean literal processing
-        //         return {
-        //             type: 'bool',
-        //             value: Boolean(reg),
-        //         };
-        //     } else if (isVar.test(reg)) {
-        //         // Variable processing
-        //         const results = TNTSymbolTable[reg].value;
-        //         const result: value = {
-        //             type: JsTypeToTNTType(typeof (results)),
-        //             value: results,
-        //         };
-        //         return result;
-        //     } else if (isMathGex.test(reg)) {
-        //         //TODO: 表达式
-        //     } else if (isXML.test(reg)) {
-        //         const result: value = {
-        //             type: 'XML',
-        //             value: reg,
-        //         }
-        //         return result;
-        //     }
         }
+
+        // ------------------------------------------------------------------------
 
         renderDOM(HTML: string, DOM: HTMLElement) {
             DOM.innerHTML = HTML;
@@ -318,7 +173,6 @@ namespace TNTScript {
         evaluate(expression: string): any {
             // TODO: evaluate the value
         }
+
     }
-
 }
-
--- src/tntscript/lexicalAnalysis.ts
+++ src/tntscript/lexicalAnalysis.ts
@@ -2,150 +2,150 @@ namespace TNTScript {
 
     // This function splits code with ";"
     export function codeSplit(code: string) {
-        // let ignoreNext = false;
-        // let stringIgnoreNext = false;
-        // const buffer = [];
-        // let currentString = "";
-        // // Iterate every charaters in the code
-        // for (const i of code) {
-        //     // Next will be ignored (In this case, means in a string.)
-        //     if (ignoreNext) {
-        //         // Add the current char to the string.
-        //         currentString += i;
-        //         if (i === '\\') {
-        //             // Escaping characters
-        //             stringIgnoreNext = true;
-        //             continue;
-        //         }
-        //         if (i === '"' && !stringIgnoreNext) {
-        //             // End the string.
-        //             ignoreNext = false;
-        //         }
-        //         if (stringIgnoreNext) {
-        //             stringIgnoreNext = false;
-        //         }
-        //         continue;
-        //     } else {
-        //         if (i === ';') {
-        //             buffer.push(currentString);
-        //             currentString = "";
-        //         } else if (i === '"') {
-        //             currentString += i;
-        //             ignoreNext = true;
-        //         } else {
-        //             currentString += i;
-        //         }
-        //     }
-        // }
-        // if (currentString !== "") {
-        //     buffer.push(currentString);
-        //     currentString = "";
-        // }
-        // return buffer;
+        let ignoreNext = false;
+        let stringIgnoreNext = false;
+        const buffer = [];
+        let currentString = "";
+        // Iterate every charaters in the code
+        for (const i of code) {
+            // Next will be ignored (In this case, means in a string.)
+            if (ignoreNext) {
+                // Add the current char to the string.
+                currentString += i;
+                if (i === '\\') {
+                    // Escaping characters
+                    stringIgnoreNext = true;
+                    continue;
+                }
+                if (i === '"' && !stringIgnoreNext) {
+                    // End the string.
+                    ignoreNext = false;
+                }
+                if (stringIgnoreNext) {
+                    stringIgnoreNext = false;
+                }
+                continue;
+            } else {
+                if (i === ';') {
+                    buffer.push(currentString);
+                    currentString = "";
+                } else if (i === '"') {
+                    currentString += i;
+                    ignoreNext = true;
+                } else {
+                    currentString += i;
+                }
+            }
+        }
+        if (currentString !== "") {
+            buffer.push(currentString);
+            currentString = "";
+        }
+        return buffer;
     }
 
     export function codeBlock(code) {
-        // const v: string = code.replace(/\{.+\}/,'');
-        // const codes: string = /\{.+\}/.exec(code)[0];
-        // return [v,codeSplit(codes.substring(1,codes.length - 1))];
+        const v: string = code.replace(/\{.+\}/,'');
+        const codes: string = /\{.+\}/.exec(code)[0];
+        return [v,codeSplit(codes.substring(1,codes.length - 1))];
     }
 
     export function keySearch(key: string,code: string) {
-        // return code.replace(new RegExp(`${key} `),'')
+        return code.replace(new RegExp(`${key} `),'')
     }
 
     export function functionSplit(code: string, original: boolean = false) {
-        // let ignoreNext = false;
-        // let stringIgnoreNext = false;
-        // let bracketMatchingMode = false;
-        // let bracketMatchingStack = [];
-        // const buffer = [];
-        // let currentString = "";
-        // // Iterate every charaters in the code
-        // for (const i of code) {
-        //     // Bracket Matching Mode
-        //     if (bracketMatchingMode) {
-        //         currentString += i;
-        //         if (i === '}') {
-        //             bracketMatchingStack.pop();
-        //             if (bracketMatchingStack.length === 0) {
-        //                 buffer.push(currentString + "}");
-        //                 bracketMatchingMode = false;
-        //                 continue;
-        //             }
-        //         } else if (i === '{') {
-        //             bracketMatchingStack.push(null);
-        //         }
-        //         continue;
-        //     }
-        //     // Next will be ignored (In this case, means in a string.)
-        //     if (ignoreNext) {
-        //         // Add the current char to the string.
-        //         currentString += i;
-        //         if (i === '\\') {
-        //             // Escaping characters
-        //             stringIgnoreNext = true;
-        //             continue;
-        //         }
-        //         if (i === '"' && !stringIgnoreNext) {
-        //             // End the string.
-        //             ignoreNext = false;
-        //         }
-        //         if (stringIgnoreNext) {
-        //             stringIgnoreNext = false;
-        //         }
-        //         continue;
-        //     } else {
-        //         if (i === ',') {
-        //             buffer.push(currentString.trim());
-        //             currentString = "";
-        //         } else if (i === '"') {
-        //             currentString += i;
-        //             ignoreNext = true;
-        //         } else if (i === '{') {
-        //             // Right bracket
-        //             bracketMatchingMode = true;
-        //             bracketMatchingStack = [null];
-        //             currentString += i;
-        //         } else {
-        //             currentString += i;
-        //         }
-        //     }
-        // }
-        // if (currentString !== "") {
-        //     buffer.push(currentString.trim());
-        //     currentString = "";
-        // }
-        // const values = { agv: [], functioncanvalue: {} };
-        // for (const value of buffer) {
-        //     if (/.+ ?= ?.+/.test(value)) {
-        //         const v = /[^= ]+/.exec(/= ?.+/.exec(code).join(' '));
-        //         const name = /[^? =]/.exec(/([A-z0-9])+ ?=/.exec(code).join(' '));
-        //         values.functioncanvalue[name[0]] = new ScriptExecutor().ValueProcess(v[0]);
-        //     } else {
-        //         if (original) {
-        //             values.agv.push(new ScriptExecutor().ValueProcess(value));
-        //         } else {
-        //             values.agv.push(value)
-        //         }
-        //     }
-        // }
-        // return values;
+        let ignoreNext = false;
+        let stringIgnoreNext = false;
+        let bracketMatchingMode = false;
+        let bracketMatchingStack = [];
+        const buffer = [];
+        let currentString = "";
+        // Iterate every charaters in the code
+        for (const i of code) {
+            // Bracket Matching Mode
+            if (bracketMatchingMode) {
+                currentString += i;
+                if (i === '}') {
+                    bracketMatchingStack.pop();
+                    if (bracketMatchingStack.length === 0) {
+                        buffer.push(currentString + "}");
+                        bracketMatchingMode = false;
+                        continue;
+                    }
+                } else if (i === '{') {
+                    bracketMatchingStack.push(null);
+                }
+                continue;
+            }
+            // Next will be ignored (In this case, means in a string.)
+            if (ignoreNext) {
+                // Add the current char to the string.
+                currentString += i;
+                if (i === '\\') {
+                    // Escaping characters
+                    stringIgnoreNext = true;
+                    continue;
+                }
+                if (i === '"' && !stringIgnoreNext) {
+                    // End the string.
+                    ignoreNext = false;
+                }
+                if (stringIgnoreNext) {
+                    stringIgnoreNext = false;
+                }
+                continue;
+            } else {
+                if (i === ',') {
+                    buffer.push(currentString.trim());
+                    currentString = "";
+                } else if (i === '"') {
+                    currentString += i;
+                    ignoreNext = true;
+                } else if (i === '{') {
+                    // Right bracket
+                    bracketMatchingMode = true;
+                    bracketMatchingStack = [null];
+                    currentString += i;
+                } else {
+                    currentString += i;
+                }
+            }
+        }
+        if (currentString !== "") {
+            buffer.push(currentString.trim());
+            currentString = "";
+        }
+        const values = { agv: [], functioncanvalue: {} };
+        for (const value of buffer) {
+            if (/.+ ?= ?.+/.test(value)) {
+                const v = /[^= ]+/.exec(/= ?.+/.exec(code).join(' '));
+                const name = /[^? =]/.exec(/([A-z0-9])+ ?=/.exec(code).join(' '));
+                values.functioncanvalue[name[0]] = new ScriptExecutor().ValueProcess(v[0]);
+            } else {
+                if (original) {
+                    values.agv.push(new ScriptExecutor().ValueProcess(value));
+                } else {
+                    values.agv.push(value)
+                }
+            }
+        }
+        return values;
     }
 
     export function jsTypeToTNTType(TypeName) {
-        // switch (TypeName) {
-        //     case "String":
-        //         return 'string';
-        //         break;
-        //     case "Number":
-        //         return 'number';
-        //         break;
-        //     case "Boolean":
-        //         return 'bool';
-        //         break;
-        //     default:
-        //         break;
-        // }
+        switch (TypeName) {
+            case "String":
+                return 'string';
+                break;
+            case "Number":
+                return 'number';
+                break;
+            case "Boolean":
+                return 'bool';
+                break;
+            default:
+                break;
+        }
     }
 }
