da426f50defc26f2b80a5c8e013f33e9eb2b170f
27Onion Nebell <57032603+onion108@users.noreply.github.com>
2022-05-29 20:48:20 +0800

    fix: eslint

--- src/runtime/VTagRenderer.ts
+++ src/runtime/VTagRenderer.ts
@@ -13,13 +13,13 @@ namespace TNT {
         }
         private defaultRenderer(s: string): string {
             try {
-                return `${Globals.evaluate(s)}`
+                return `${Globals.evaluate(s)}`;
             } catch (e) {
-                return `Error while rendering element: ${e}`
+                return `Error while rendering element: ${e}`;
             }
         }
         render() {
-            const vTags = document.querySelectorAll('v');
+            const vTags = document.querySelectorAll("v");
             for (const i of vTags) {
                 // For each elements
                 const rendered = i.getAttribute("data-rendered");
@@ -34,7 +34,7 @@ namespace TNT {
                     // In this case, we should check if the tag really should be rendered or not.
                     // This is called the DIFF CHECK
                     const content = i.getAttribute("data-original");
-                    let newRenderedContent = this.customRenderer?.(content) ?? this.defaultRenderer(content);
+                    const newRenderedContent = this.customRenderer?.(content) ?? this.defaultRenderer(content);
                     // Compare if the element should be rerendered.
                     if (i.innerHTML !== newRenderedContent) {
                         // It's time to rerender it.
