b02af152579048ef0847dda173838159eb90c380
27Onion Nebell <57032603+onion108@users.noreply.github.com>
2022-05-29 20:52:47 +0800

    fix: eslint

--- src/runtime/TNT.ts
+++ src/runtime/TNT.ts
@@ -18,7 +18,7 @@ namespace TNT {
         private prv_checkOptionTags() {
 
             // Debug mode.
-            let debugModeOptionTags = document.querySelectorAll("tnt-debug");
+            const debugModeOptionTags = document.querySelectorAll("tnt-debug");
             if (debugModeOptionTags.length === 0) {
                 // Didn't enable the debug mode.
                 if (window.location.href.startsWith("file:")) {
@@ -26,34 +26,34 @@ namespace TNT {
                     console.warn("Warning: It seems that you are developing the webpage but you don't enable the debug mode.\nIt's better for you to enable the debug mode by the html tag <tnt-debug></tnt-debug> to enable more debugging features.");
                     console.warn("If your application is designed to run on file:/// protocal, please ignore this warning.");
                 }
-                Globals.removePlugin('tntdebug');
+                Globals.removePlugin("tntdebug");
                 this.prv_isDebug = false;
             }
 
             // Disable the tnt script feature.
-            let noTNTScriptTags = document.querySelectorAll("tnt-no-script");
+            const noTNTScriptTags = document.querySelectorAll("tnt-no-script");
             if (noTNTScriptTags.length !== 0) {
                 console.warn("Warning: Disabling TNT script may cause some unexpected results. If you're sure you want to disabl the TNT Script feature, please ignore this warning.");
-                Globals.removePlugin('tntscript');
+                Globals.removePlugin("tntscript");
             }
 
             // Disable other plugins that required to be disabled.
-            let disablePluginTags = document.querySelectorAll('tnt-disable-plugin');
+            const disablePluginTags = document.querySelectorAll("tnt-disable-plugin");
             for (const tag of disablePluginTags) {
-                const pluginId = tag.getAttribute('plugin');
+                const pluginId = tag.getAttribute("plugin");
                 if (pluginId !== null) {
                     Globals.removePlugin(pluginId);
                 }
             }
 
             // Pure mode.
-            let pureModeTags = document.querySelectorAll('tnt-pure-mode');
-            let noPluginModeTags = document.querySelectorAll('tnt-no-plugin');
+            const pureModeTags = document.querySelectorAll("tnt-pure-mode");
+            const noPluginModeTags = document.querySelectorAll("tnt-no-plugin");
             if (pureModeTags.length !== 0 || noPluginModeTags.length !== 0) {
-                console.warn('Warning: You disabled all the plugins, including the TNT Script plugin and TNT Debugger plugin! Are you sure that\'s what you want? If not, please turn off the Pure Mode option. ');
-                console.log('Hint: Use <tnt-disable-plugin plugin="plugin_id_to_delete"></tnt-disable-plugin> to disable a single plugin. \nUse <tnt-no-script></tnt-disable-plugin> to disable the TNT Script integrated plugin (equal to <tnt-disable-plugin plugin="tntscript"></tnt-disable-plugin>). Remove the <tnt-debug></tnt-debug> tag to disable the debugger plugin.');
+                console.warn("Warning: You disabled all the plugins, including the TNT Script plugin and TNT Debugger plugin! Are you sure that's what you want? If not, please turn off the Pure Mode option. ");
+                console.log("Hint: Use <tnt-disable-plugin plugin=\"plugin_id_to_delete\"></tnt-disable-plugin> to disable a single plugin. \nUse <tnt-no-script></tnt-disable-plugin> to disable the TNT Script integrated plugin (equal to <tnt-disable-plugin plugin=\"tntscript\"></tnt-disable-plugin>). Remove the <tnt-debug></tnt-debug> tag to disable the debugger plugin.");
                 // Disable all the plugins.
-                let pluginNames: string[] = [];
+                const pluginNames: string[] = [];
                 for (const plugin of Globals.getAllPlugins()) {
                     pluginNames.push(plugin.id);
                 }
@@ -64,11 +64,11 @@ namespace TNT {
 
             // ----------------------------------------------------------------
             // EASTER EGGS OPTIONS
-            let flipModeTags = document.querySelectorAll('tnt-flip');
+            const flipModeTags = document.querySelectorAll("tnt-flip");
             if (flipModeTags.length !== 0) {
                 // Flip mode on
-                window.addEventListener('load', () => {
-                    document.querySelector('html').style.setProperty('transform', 'scaleX(-1)');
+                window.addEventListener("load", () => {
+                    document.querySelector("html").style.setProperty("transform", "scaleX(-1)");
                 });
             }
             // ----------------------------------------------------------------
