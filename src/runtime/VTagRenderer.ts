a31a94dc8f9544dfc9cd35aad8cd692dd63b6d77
Sam Zhang <samzhangjy@outlook.com>
2022-06-08 08:35:07 +0800

    refactor: reverted VTagRenderer refactor
    Refactoring will be continued in the `dev` branch.

--- src/runtime/VTagRenderer.ts
+++ src/runtime/VTagRenderer.ts
@@ -5,35 +5,45 @@
  * description: Renderer for the V tag.
  */
 
-export const defaultRenderer = (s: string): string => {
-  try {
-    return `${Globals.evaluate(s)}`;
-  } catch (e) {
-    return `Error while rendering element: ${e}`;
-  }
-};
-
-export const render = (customRenderer?: (vTagContent: string) => string) => {
-  const vTags = document.querySelectorAll("v");
-  const renderer = customRenderer ?? defaultRenderer;
-  for (const tag of vTags) {
-    const rendered = tag.getAttribute("data-rendered");
-    if (rendered === null) {
-      // Not rendered
-      // In this case, the tag always should be rendered.
-      tag.setAttribute("data-rendered", "YES");
-      tag.setAttribute("data-original", tag.innerHTML);
-      tag.innerHTML = customRenderer?.(tag.innerHTML) ?? defaultRenderer(tag.innerHTML);
-      return;
+ namespace TNT {
+  export class VTagRenderer {
+    private customRenderer?: (vTagContent: string) => string;
+    constructor(customRenderer: (vTagContent: string) => string = undefined) {
+      this.customRenderer = customRenderer;
     }
-    // Already rendered
-    // In this case, we should check if the tag really should be rendered or not.
-    // This is called the DIFF CHECK
-    const content = tag.getAttribute("data-original");
-    const newRenderedContent = renderer(content);
-    // Compare if the element should be rerendered.
-    if (tag.innerHTML !== newRenderedContent) {
-      tag.innerHTML = newRenderedContent;
+    private defaultRenderer(s: string): string {
+      try {
+        return `${Globals.evaluate(s)}`;
+      } catch (e) {
+        return `Error while rendering element: ${e}`;
+      }
+    }
+    render() {
+      const vTags = document.querySelectorAll("v");
+      for (const i of vTags) {
+        // For each elements
+        const rendered = i.getAttribute("data-rendered");
+        if (rendered === null) {
+          // Not rendered
+          // In this case, the tag always should be rendered.
+          i.setAttribute("data-rendered", "YES");
+          i.setAttribute("data-original", i.innerHTML);
+          i.innerHTML = this.customRenderer?.(i.innerHTML) ?? this.defaultRenderer(i.innerHTML);
+        } else {
+          // Already rendered
+          // In this case, we should check if the tag really should be rendered or not.
+          // This is called the DIFF CHECK
+          const content = i.getAttribute("data-original");
+          const newRenderedContent = this.customRenderer?.(content) ?? this.defaultRenderer(content);
+          // Compare if the element should be rerendered.
+          if (i.innerHTML !== newRenderedContent) {
+            // It's time to rerender it.
+            i.innerHTML = newRenderedContent;
+          } else {
+            // Else, just do nothing here.
+          }
+        }
+      }
     }
   }
-};
+}
